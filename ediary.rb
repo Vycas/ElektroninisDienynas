1a124c13060506abffe965429a0cec9335b96189
Vycas <vycas0@gmail.com>
2009-10-31 03:30:07 +0200

    Corrected indentation

--- ediary.rb
+++ ediary.rb
@@ -4,82 +4,82 @@ require 'user'
 require 'admin'
 
 class EDiary
-    def initialize
-        User.users['admin'] = Admin.new('admin', 'boom')
-        User.users['vycas'] = Student.new('vycas', 'vycas')
-        User.users['ruby'] = Teacher.new('ruby', 'ruby')
-    end
-
-    def welcome
-        puts '-' * 40
-        puts 'Welcome to electronic diary.'
-        puts 'Version: 1.0'
-        puts '-' * 40
-    end
-
-    def login
-        load_state
-
-        print 'Login: '
-        login = gets.chomp
-        print 'Password: '
-        password = gets.chomp
-
-        if user = User.users[login]
-            if user.password == password
-                go(user)
-                save_state
-            else
-                puts 'Incorrect password!'
-            end
-        else
-            puts 'No such user!'
-        end
-    end
-
-  def go(user)
-    puts 'You have logged in as ' + user.class.to_s
-    puts 'Type "disconnect" to quit. Type "help" to get info about commands.'
-    while true
-      print '> '
-      cmd = gets.chomp
-      break if cmd == 'disconnect'
-      parts = cmd.split
-      if user.commands.include?(parts[0].to_sym)
-        begin
-          code = 'user.' + parts[0]
-          code += " '#{parts[1]}'" if parts[1]
-          parts[2..-1].each { |p| code += ", '#{p}'" } if parts[2]
-          eval(code)
-        rescue Exception
-          puts 'Error occured!'
-        end
-      else
-        puts 'Incorrect command!'
-      end
-    end
-    puts 'Disconnected'
+  def initialize
+    User.users['admin'] = Admin.new('admin', 'boom')
+    User.users['vycas'] = Student.new('vycas', 'vycas')
+    User.users['ruby'] = Teacher.new('ruby', 'ruby')
   end
 
-    private
+  def welcome
+    puts '-' * 40
+    puts 'Welcome to electronic diary.'
+    puts 'Version: 1.0'
+    puts '-' * 40
+  end
 
-    Storage = "storage.data"
+  def login
+    load_state
 
-    def load_state
-        if File.exists? Storage
-            f = File.open(Storage, 'r')
-            d = f.read(File.size(Storage))
-            User.users = Marshal.load(d)
-            f.close
-        end
+    print 'Login: '
+    login = gets.chomp
+    print 'Password: '
+    password = gets.chomp
+
+    if user = User.users[login]
+      if user.password == password
+        go(user)
+        save_state
+      else
+        puts 'Incorrect password!'
+      end
+    else
+      puts 'No such user!'
     end
+  end
 
-    def save_state
-        f = File.new(Storage, 'w')
-        d = Marshal.dump(User.users)
-        f.write(d)
-        f.close
+  def go(user)
+  puts 'You have logged in as ' + user.class.to_s
+  puts 'Type "disconnect" to quit. Type "help" to get info about commands.'
+  while true
+    print '> '
+    cmd = gets.chomp
+    break if cmd == 'disconnect'
+    parts = cmd.split
+    if user.commands.include?(parts[0].to_sym)
+    begin
+      code = 'user.' + parts[0]
+      code += " '#{parts[1]}'" if parts[1]
+      parts[2..-1].each { |p| code += ", '#{p}'" } if parts[2]
+      eval(code)
+    rescue Exception
+      puts 'Error occured!'
     end
+    else
+    puts 'Incorrect command!'
+    end
+  end
+  puts 'Disconnected'
+  end
+
+  private
+
+  Storage = "storage.data"
+
+  def load_state
+    if File.exists? Storage
+      f = File.open(Storage, 'r')
+      d = f.read(File.size(Storage))
+      User.users = Marshal.load(d)
+      f.close
+    end
+  end
+
+  def save_state
+    f = File.new(Storage, 'w')
+    d = Marshal.dump(User.users)
+    f.write(d)
+    f.close
+  end
 end
 
 diary = EDiary.new
