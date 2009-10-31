39f8ac561729e48a080da2956ac6497e1e8583ac
Vycas <vycas0@gmail.com>
2009-10-31 14:01:06 +0200

    Added course class

---
+++ course.rb
@@ -0,0 +1,19 @@
+class Course
+  def initialize(teacher, title=nil, description=nil, time=nil)
+    if title
+      @title = title
+      @description = description
+      @time = time
+    else
+      puts 'Enter course title: '
+      @title = gets.chomp
+      puts 'Enter course description: '
+      @description = gets.chomp
+      puts 'Enter course time: '
+      @time = gets.chomp
+    end
+    @teacher = teacher
+  end
+
+  attr_reader :title, :description, :time, :teacher
+end
---
+++ course_spec.rb
@@ -0,0 +1,26 @@
+require 'course'
+require 'teacher'
+
+describe Course do
+  before(:each) do
+    teacher = Teacher.new('Teacher', 'password')
+    @course = Course.new(teacher, 'Math', 'Science of numbers', 'Monday 8.00')
+  end
+
+  it 'should have title' do
+    @course.should respond_to(:title)
+  end
+
+  it 'should have description' do
+    @course.should respond_to(:description)
+  end
+
+  it 'should have time' do
+    @course.should respond_to(:time)
+  end
+
+  it 'should have assigned teacher' do
+    @course.should respond_to(:teacher)
+    @course.teacher.should be_instance_of(Teacher)
+  end
+end
