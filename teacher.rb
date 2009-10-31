c5e9f0a922c9fa96b8f13f539fc02cbf3e4db5ca
Vycas <vycas0@gmail.com>
2009-10-31 15:13:03 +0200

    Added help command for teacher

--- teacher.rb
+++ teacher.rb
@@ -7,7 +7,15 @@ class Teacher < User
   end
 
   def commands
-    [:change_password, :add_course, :remove_course, :list_courses]
+    [:help, :change_password, :add_course, :remove_course, :list_courses]
+  end
+
+  def help
+    out =  "Available commands:\n"
+    out << "  add_course <title> [<description>] [<time>] - to add new course\n"
+    out << "  remove_course <title> - to remove existing course\n"
+    out << "  list_courses - to list existing courses\n"
+    out << "  change_password <password> - to change your password\n"
   end
 
   def add_course(title, description=nil, time=nil)
